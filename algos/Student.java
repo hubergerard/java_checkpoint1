3eda36b4b05555c33bdc460b42bc81f14b039f82
Stefan Ganze <stefan.ganze@googlemail.com>
2019-12-12 11:33:29 +0100

    Students - Methoden geschrieben

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,34 @@
 public class Student {
 
-    // TODO hasLegalAge
+	// TODO hasLegalAge
+	public static boolean hasLegalAge(int age) {
+		if (age >= 18) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    // TODO getGroup
+	// TODO getGroup
+	public static char getGroup(int number) {
+		if (number % 2 == 0) {
+			return 'A';
+		} else {
+			return 'B';
+		}
+	}
 
-    // TODO countStudents
+	// TODO countStudents
+	public static int countStudents(String[] language) {
+		int counter = 0;
+
+		for (int i = 0; i < language.length; i++) {
+
+			if (language[i].equalsIgnoreCase("Java")) {
+				counter++;
+			}
+		}
+		return counter;
+	}
 
 }
