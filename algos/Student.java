82365a08701eb921820546a98ad742d54d7e290f
Michael Berger <michael.goetz.berger@telekom.de>
2019-12-12 10:04:25 +0100

    II. Java / algorithms

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,35 @@
 public class Student {
 
-    // TODO hasLegalAge
+	public static boolean isLegal(int age) {
+		return age >= 18;
+	}
 
-    // TODO getGroup
-
-    // TODO countStudents
-
+	public static boolean hasLegalAge(int age) {
+		return Student.isLegal(age);
+	}
+	
+	static char giveGroup(int studentNumber) {
+		if (studentNumber % 2 == 0) {
+			return 'A';
+		} else {
+			return 'B';
+		}
+	}
+	
+	static char getGroup(int studentNumber) {
+		return giveGroup(studentNumber);
+	}
+	
+	static int countStudents(String[] languages) {
+		int count = 0;
+		
+		for (String language : languages) {
+			if (language.equalsIgnoreCase("java")) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
 }
