9b16d4826f60d3108fcf977ab1a9b743f44e58ec
Alexander Kirsch <a.kirsch@gmx.de>
2019-12-12 14:26:16 +0100

    Zwischenstand

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,40 @@
 public class Student {
 
-    // TODO hasLegalAge
-
+// TODO hasLegalAge
+public static boolean hasLegalAge (int age) {
+if (age>18) { return true;}
+else
+{return false;}
+}
     // TODO getGroup
 
+public static char getGroup (int number) {
+int i=number%2;
+if (i==0)
+{return 'A';}
+else
+{return 'B';}
+}
     // TODO countStudents
+
+public static int countStudents (String [] languages) {
+int anzahlEintrage = languages.length;
+int j=0; // Anzahl der Java-Einträge
+String kleinbuchstaben = "";
+for (int i=0; i<anzahlEintrage; i++) {
+kleinbuchstaben=languages[i].toLowerCase​();
+if (kleinbuchstaben.equals("java"))
+{j=j+1;}
+}
+
+return j;
+
+
+}
+
+
+
+
+
 
 }
---
+++ algos/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+public static void main (String [] args) {
+
+String [] languages={"Java", "java", "JaVa", "jAvA", "sldfk", "daskf"};
+  System.out.println(Student.countStudents(languages));
+
+}
+}
