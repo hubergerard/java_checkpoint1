e15e2b18851d64cf994276a6ca644e4f521f6663
Michael Blisner <Michael.Blisner@telekom.de>
2019-12-12 10:29:00 +0100

    Java Aufgabe

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,40 @@
 public class Student {
 
-    // TODO hasLegalAge
+	// TODO hasLegalAge
+	public static boolean hasLegalAge(int age) {
 
-    // TODO getGroup
+		if (age > 18) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    // TODO countStudents
+	// TODO getGroup
+	public static char getGroup(int number) {
+
+		if (number % 2 == 0) {
+			System.out.println("The number is even.");
+			return 'A';
+		} else {
+			System.out.println("The number is odd.");
+			return 'B';
+		}
+	}
+
+	// TODO countStudents
+	public static int countStudents(String[] students) {
+
+		String lowCase = "";
+		int number = 0;
+
+		for (int i = 0; i < students.length; i++) {
+			lowCase = students[i].toLowerCase();
+			if (lowCase.equals("java")) {
+				number++;
+			}
+		}
+		return number;
+	}
 
 }
