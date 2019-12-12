ea17c268ea5bb590daf78d2b8ec70b1a74778c13
Malte Knezevic <m.knezevic@telekom.de>
2019-12-12 12:32:50 +0100

    Die ersten Aufgaben beendet

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,44 @@
 public class Student {
 
-    // TODO hasLegalAge
+   
+      public static boolean hasLegalAge(int age){
+ 	 if(age<18){
+		 return false;
+	 }
+	 else{
+		 return true;
+		}
+	}       
+	// TODO hasLegalAge
+
+
+
+	public static char getGroup (int number){
+	  if(number%2 == 0){
+	   return 'A';
+	   }	   
+	 else{
+          return 'B';
+	 }
+	}
 
     // TODO getGroup
 
+
+	public static int countStudents (String [] array){
+	 
+		int count = 0;
+
+		for(int i=0; i<array.length;i++){
+		  array[i] = array[i].toLowerCase();
+		  if(array[i].equals ("java")){
+			count+=1;
+		}
+
+	}    
+
+	return count;
     // TODO countStudents
 
+}
 }
