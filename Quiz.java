c6a1ec8f5cc334c67be449e7dfb160ee4e97c8f0
mkrill <michael.krill@telekom.de>
2019-12-12 10:23:04 +0100

    quiz answered

--- Quiz.java
+++ Quiz.java
@@ -22,44 +22,6 @@ public class Quiz {
 	private static final String ANSWER_TEXT1 = "\nAnswer:";
 	private static final String OUTRO_TEXT1 = "\nThe %s file has been generated, you can edit it manually if you want to change it.\n";
 
-	public static boolean isLegal(int age) {
-		// Create a static method named isLegal, which returns "true" if the age passed
-		// as an argument is greater than or equal to 18 and returns "false" if it is
-		// not.
-		return (age >= 18) ? true : false;
-	}
-
-	public static char giveGroup(int studNumber) {
-
-		// Create a static method called giveGroup, which takes in a student's number.
-		// If this is even, return the character "A", otherwise return the character
-		// "B".
-
-		return (studNumber % 2 == 0) ? 'A' : 'B';
-
-	}
-
-	public static int countStudents(String[] languages) {
-
-		// Create a static method called countStudents, which receives an array
-		// containing the languages chosen by the candidates. Return the number of
-		// candidates who have chosen Java, regardless of how they wrote it (upper/lower
-		// case).
-
-		int javaStudentCounter = 0;
-
-		if (languages != null) {
-			for (String language : languages) {
-				if (language.toLowerCase() == "java") {
-					javaStudentCounter++;
-				}
-			}
-		}
-
-		return javaStudentCounter;
-
-	}
-
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
