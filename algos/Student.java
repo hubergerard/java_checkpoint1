f749b30d21aac6bd702d049eebcd8180cad6b120
Thoralf Friedrich <thoralf.friedrich@telekom.de>
2019-12-12 16:07:22 +0100

    Lösung

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,39 @@
 public class Student {
 
     // TODO hasLegalAge
+    public static boolean hasLegalAge(int age) {
+        if (age >= 18){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     // TODO getGroup
+    public static char getGroup(int number){
+        
+        if (number % 2 == 0){
+            return 'A';
+        }
+        else {
+            return 'B';
+        }
+    }
 
     // TODO countStudents
+    public static int countStudents(String [] arg){
+        int counter = 0;
+        int i;
+        String wort = "";
+    
+        for (i = 0; i < arg.length; i++){
+            wort = arg[i].toLowerCase();
 
+            if (wort.equals ("java")){
+                counter++;
+            }
+        }
+        return counter;
+    }
 }
