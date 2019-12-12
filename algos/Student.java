66ae49b0579b7d22c777593c1845aa4565fc907c
Denis Kutlovci <deniskutlovci@hotmail.com>
2019-12-12 13:29:17 +0100

    Added two methods

--- algos/Student.java
+++ algos/Student.java
@@ -1,8 +1,29 @@
+
 public class Student {
 
     // TODO hasLegalAge
+    public static boolean isLegal(int age){
+    	if (age >=18){
+    		return true;
+    	}else{
+    		return false;
+    	}	
+    }
+
+
+
 
     // TODO getGroup
+    public static char giveGroup (int studentNumber){
+        if (studentNumber % 2 == 0){
+            return 'A';
+        }else {
+            return 'B';
+        }
+    }
+
+
+
 
     // TODO countStudents
 
