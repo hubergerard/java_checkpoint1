79997b666eba5b2baf41b46335163addc72f69cd
Michael Obst <michael.obst@telekom.de>
2019-12-12 10:00:51 +0100

    algorithm methods coded

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,26 @@
 public class Student {
 
-    // TODO hasLegalAge
+	// TODO hasLegalAge
+	public static boolean isLegal(int age) {
+		return (age >= 18) ? true : false;
+	}
 
-    // TODO getGroup
+	// TODO getGroup
+	public static char giveGroup(int studentNumber) {
+		return (studentNumber % 2 == 0) ? 'A' : 'B';
+	}
 
-    // TODO countStudents
+	// TODO countStudents
+	public static int countStudents(String[] studentSurvey) {
+		int studentCount = 0;
 
+		if (studentSurvey != null) {
+			for (String studentAnswer : studentSurvey) {
+				if (studentAnswer.equalsIgnoreCase("java")) {
+					studentCount++;
+				}
+			}
+		}
+		return studentCount;
+	}
 }
