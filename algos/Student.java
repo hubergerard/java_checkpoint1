c6939c643a878b6b67dfc61a910c6aa6bef5a761
Claus-Peter Schneider <Claus-Peter.Schneider@telekom.de>
2019-12-12 12:12:57 +0100

    Timeout

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,29 @@
 public class Student {
 
-    // TODO hasLegalAge
+	// TODO hasLegalAge
 
-    // TODO getGroup
+	// TODO getGroup
 
-    // TODO countStudents
+	// TODO countStudents
+	public static void isLegal (int age) {
+	for (int i = 0; i <isLegal.length; i++)
+	{
+		if (islegal(age) >= 18{
+			return true;
+		}
+			else {
+				return false;	
+				}
+	}
 
+	public static void giveGroup (String group) {
+		int student = 0; 
+		for (int i = 0; i <group.length; i++),{
+					if (student%2 ==0)
+				return "A";
+		}
+				else {
+					return "B";	
+					}
+		}
 }
