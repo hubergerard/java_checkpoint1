4229a1738db20afd695e48ded7dce7dd0bb24bc6
Markus Heinrichs <markus@heinrichs.es>
2019-12-12 15:43:32 +0100

    Worked on Checkpoint 1 quest

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,20 @@
 public class Student {
 
-    // TODO hasLegalAge
+	public static boolean hasLegalAge(int age) {
+		return age < 18 ? false : true;
+	}
 
-    // TODO getGroup
+	public static char getGroup(int number) {
+		return (number % 2 == 0) ? 'A' : 'B';
+	}
 
-    // TODO countStudents
+	public static int countStudents(String[] languages) {
+		int result = 0;
+		for (String lang : languages) {
+			if (lang.equalsIgnoreCase("java"))
+				result++;
+		}
+		return result;
+	}
 
 }
