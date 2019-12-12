094f4d9aca936cf2c424bf0a95cdb0a611d21e18
Christoph Abert <christoph.abert@telekom.de>
2019-12-12 11:03:17 +0100

    start

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,22 @@
 public class Student {
 
     // TODO hasLegalAge
+    public static String isLegal (int age) {
+      if (age >= 18) {
+        return ("true");
+      }
+      else {
+        return ("false");
+      }
+    }
 
     // TODO getGroup
 
     // TODO countStudents
 
+    public static void main (String[] args) {
+
+System.out.println(isLegal (17). isLegal (18));
+
+}
 }
