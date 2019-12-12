21022559057a5bddfc6f2eeb28c344b2e05e99cc
zaphodess <monika.messerer@telekom.de>
2019-12-12 12:37:52 +0100

    Hopefully the correct file in the correct branch now ;-)

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,31 @@
 public class Student {
 
     // TODO hasLegalAge
+    static boolean hasLegalAge (int age){
+      if (age>=18){
+      return true;}
+      else {return false;}
+    }
 
     // TODO getGroup
+    static char getGroup (int student_number){
+      char groupId='A';
+      if (student_number % 2==1){groupId='B';}
+    return groupId;
+    }
 
     // TODO countStudents
+    static int countStudents (String[] languages){
+      int javaCandidates=0;
+      for (String i : languages){
+        i=i.toLowerCase();
+        /* check of length because JavaScript must not be counted */
+        if (i.length()<6 && i.contains("java")){
+          javaCandidates+=1;
+         }
+      }
+      return javaCandidates;
+    }
+
 
 }
