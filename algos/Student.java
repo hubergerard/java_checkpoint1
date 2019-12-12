5d262b8022bfe24b2f0d92486a3995194717d997
Axcce1erat <axelschwarz@gmx.de>
2019-12-12 12:07:19 +0100

    first save

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,20 @@
+package algos;
+
 public class Student {
 
     // TODO hasLegalAge
-
+    public boolean isLegal (int age){
+        if (age >= 18){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     // TODO getGroup
+    public void giveGroup (int number){
 
+    }
     // TODO countStudents
 
 }
