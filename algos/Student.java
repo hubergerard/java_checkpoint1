11c36e2e4e8a9c1bf0f13fd6890b313623c9c242
Metje Ruberg <metje.ruberg@telekom.de>
2019-12-12 10:04:51 +0100

    Java fertig

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,31 @@
 public class Student {
 
-    // TODO hasLegalAge
+	// TODO hasLegalAge
+	public static boolean hasLegalAge(int age) {
+		if (age >= 18)
+			return true;
+		else
+			return false;
+	}
 
-    // TODO getGroup
+	// TODO getGroup
+	public static char getGroup(int studentNumber) {
+		if ((studentNumber % 2) == 0)
+			return 'A';
+		else
+			return 'B';
+	}
 
-    // TODO countStudents
+	// TODO countStudents
+	public static int countStudents(String[] aChoosenLanguage) {
+		int anz = 0;
+		for (String language : aChoosenLanguage) {
+
+			if (language.toUpperCase().equals("JAVA"))
+				anz++;
+		}
+
+		return anz;
+	}
 
 }
