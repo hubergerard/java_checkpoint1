30be53bdc6e13ff0ee0f3a10cb1747e65116c46c
Christoph Abert <christoph.abert@telekom.de>
2019-12-12 11:43:53 +0100

    java algorithms

--- algos/Student.java
+++ algos/Student.java
@@ -1,22 +1,40 @@
 public class Student {
 
     // TODO hasLegalAge
-    public static String isLegal (int age) {
+    public static boolean hasLegalAge (int age) {
       if (age >= 18) {
-        return ("true");
+        return (true);
       }
-      else {
-        return ("false");
-      }
+      return (false);
     }
 
     // TODO getGroup
+    public static char getGroup (int number) {
+      if (number%2==0) {
+      return 'A';
+      }
+    return 'B';
+    }
 
     // TODO countStudents
+    public static int countStudents (String [] language) {
+      int count = 0;
+        for (String l : language)  {
+              if (l.equalsIgnoreCase("java")) {
+                count += 1;
+              }
+        }
+      return count;
+    }
 
-    public static void main (String[] args) {
+        /*only for testing
+        public static void main (String[] args) {
 
-System.out.println(isLegal (17). isLegal (18));
+        System.out.println(hasLegalAge (17) + ", " + hasLegalAge (18)+ ", " + hasLegalAge (-1));
+        System.out.println(getGroup (17) + ", " + getGroup (18)+ ", " + getGroup (1113)+ ", " + getGroup (-1));
+        String[] students = {"java", "js", "Java", "PHP", "JS", "JAVA", "Java"};
+        System.out.println(countStudents(students));
 
-}
+        }
+        */
 }
