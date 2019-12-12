bc9858947562e940feca1c9fb0cd1f0432caea97
Frank Geimer <frank.geimer@telekom.de>
2019-12-12 12:13:09 +0100

    Klassenarbeit

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,32 @@
 public class Student {
 
-    // TODO hasLegalAge
+	// TODO hasLegalAge
+	public static boolean isLegal(boolean Legal) {
+		int age = 0;
+		if (age >= 18)
+			return (true);
+		else {
+			return (false);
+		}
+	}
 
-    // TODO getGroup
+	// TODO getGroup
+	public static char giveGroup (int number) {
+		if (number % 2 == 0);
+		{
+		return 'A';
+		}
+		else { 
+			return 'B';
+		}
 
-    // TODO countStudents
+	// TODO countStudents
+	public static String countStudents (String [] students) {
+		if (students == "java" || students =="Java" || students =="JAVA" || students =="Java") {
+			int i=i+1;
+		}
+		return i;
+	
+	}
 
 }
