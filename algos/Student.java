71efb4b404f701015259540056832f3ef4015ce3
Arne Chrestin <arne.chrestin@telekom.de>
2019-12-12 12:10:17 +0100

    viel Spass!

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,26 @@
 public class Student {
 
-    // TODO hasLegalAge
+    // hasLegalAge
+	public static boolean hasLegalAge(int age) {
+		return (age >= 18);
+	}
 
-    // TODO getGroup
+    // getGroup
+	public static char getGroup(int number) {
+		return (number%2) ==0? 'A':'B';
+	}
 
     // TODO countStudents
+	public static int countStudents(String[] language) {
+		int z = 0; 
+		for (int i = 0; i < language.length; i++) {
+			z += "JAVA".equals(language[i].toUpperCase())? 1:0;
+		}
+		return z;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getGroup(12));
+	}
 
 }
