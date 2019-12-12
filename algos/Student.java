1a92f0514df31b79321160ef5fb7ff723cc56fd4
marcelkamp1 <marcelkamp1@gmx.de>
2019-12-12 10:05:34 +0100

    JavaQuest Done

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,24 @@
 public class Student {
 
     // TODO hasLegalAge
+	public static boolean hasLegalAge(int age) {
+		return age >= 18 ? true : false;
+	}
 
     // TODO getGroup
-
+	public static char getGroup(int studentId) {
+		return studentId % 2 == 0 ? 'A' : 'B';
+	}
     // TODO countStudents
+	public static int countStudents(String[] students) {
+		int counter = 0;
+		for(String student:students) {
+			student = student.toLowerCase();
+			if(student.equals("java")){
+				counter++;
+			}
+		}
+		return counter;
+	}
 
 }
