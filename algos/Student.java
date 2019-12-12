f9a4996fe4bd35caf745b2b27f6bb37f20a1ae8a
Martin Langenbacher <martin.langenbacher@googlemail.com>
2019-12-12 12:12:15 +0100

    ML

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,49 @@
+import java.util.ArrayList;
+
 public class Student {
+	static boolean overAgeBoolean = false;
+	static char studentChar;
+	static int javaStudents = 0;
+	String testString = "";
 
     // TODO hasLegalAge
+	public static boolean hasLegalAge(int age) {
+		overAgeBoolean = false;
+		if (age >= 18)
+			overAgeBoolean = true;
+		return overAgeBoolean;
+	}
+	
 
     // TODO getGroup
+	public static int getGroup(int number) {
+		//char A;
+		if (number % 2 == 0)
+			studentChar = 'A';
+		else
+			studentChar = 'B';
+		
+		return studentChar;
+	}
+
 
     // TODO countStudents
-
+	public static int countStudents(String[] students) {
+		int counter =0;
+		for (String student:students) {
+			student = student.toLowerCase();
+			if (student.equals("java")) {
+				counter++;
+			}
+			
+		}
+		return counter;
+		
+		
+	}	
 }
+	
+	
+
+
+
