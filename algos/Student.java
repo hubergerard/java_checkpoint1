c645fbd413091ca35e9e0f215d3fdd1fc09a98d2
Stuchlikom <stuchlikom@gmail.com>
2019-12-13 10:49:31 +0100

    Java-Methoden geschrieben

--- algos/Student.java
+++ algos/Student.java
@@ -1,9 +1,21 @@
 public class Student {
 
     // TODO hasLegalAge
+    public static boolean hasLegalAge (int age) {
+    	return age >= 18 ? true : false;
+    }
 
     // TODO getGroup
-
+    public static char getGroup (int number) {
+    	return number % 2 == 0 ? 'A' : 'B';
+    }
     // TODO countStudents
+    public static int countStudents (String[] lang) {
+    	int count = 0;
+		for (String l : lang) {
+			if (l.equalsIgnoreCase("java")) count++;
+		}
+    	return count;
+    }
 
 }
