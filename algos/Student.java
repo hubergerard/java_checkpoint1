95b2c2ce6a2537b46d39234eb239ef253a2195d2
Robert0816 <duschek@gmx.net>
2019-12-16 23:01:42 +0100

    correction

--- algos/Student.java
+++ algos/Student.java
@@ -2,34 +2,37 @@ public class Student {
 
     // TODO hasLegalAge
 
-	   public static boolean isLegal(int age)
+	   public static boolean hasLegalAge(int age)
 	    {
-	    	 if(age < 18) {
+	    	 if(age <= 18) {
 	    		 return false;
-	    	 } else	 
+	    	 } else
 	    		 return true;
-		}
-	 	
-	    public static char giveGroup(int id)
+			}
+
+
+	 	// TODO getGroup
+	    public static char getGroup(int id)
 	    {
-	    	char group  = 'a';
-	    	char group2 = 'b';
-	    	
+	    	char group  = 'A';
+	    	char group2 = 'B';
+
 	    	if(id %2 == 0) {
 	    	return group;
 	    	} else {
-			return group2;
-	    	}	
+						return group2;
+	    	}
 	    }
-	    
+	    // TODO countStudents
 	    public static int countStudents(String[] str)
-	    { 
+			{
 	    	int counter = 0;
+
 	        for (int i = 0; i < str.length; i++) {
-	          if (str[i].toLowerCase() != null) {
+						if (str[i].toLowerCase().equals("java")) {
 	                   counter++;
-	          	}  
-	        }return counter;
+	          					}
+	       }return counter;
 	    }
 
 }
