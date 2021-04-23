dbeb5c72511b069763d7457f781e5188da0f261e
Maxime Caspani <maxime.caspani@natixis.com>
2021-04-23 11:51:47 +0200

    BlackJack completed, quizz answered, html/css W.I.P

--- algos/BlackJack.java
+++ algos/BlackJack.java
@@ -8,6 +8,31 @@ public class BlackJack {
     public static String score(String[] bank, String[] player) {
         int bankSCore =0;
         int playerScore=0;
-        return "ok";
+        boolean playerHasAce = false;
+        boolean playerHasFace = false;
+        boolean bankHasFace = false;
+        boolean bankHasAce = false;
+        for (String card : player){
+            if (card.matches("[JDK]")){
+                playerHasFace=true;
+                playerScore+=10;
+            } else {
+                if(Integer.valueOf(card) == 1) playerHasAce=true;
+                if(Integer.valueOf(card) == 10) playerHasFace=true;
+                playerScore+=Integer.valueOf(card);
+            }
+        }
+        for (String card : bank){
+            if (card.matches("[JDK]")){
+                bankHasFace = true;
+                bankSCore+=10;
+            } else {
+                if(Integer.valueOf(card) == 1) bankHasAce=true;
+                if(Integer.valueOf(card) == 10) bankHasFace=true;
+                bankSCore+=Integer.valueOf(card);
+            }
+        }
+        if((playerHasAce && playerHasFace) || (bankHasAce && bankHasFace)) return "BLACKJACK!";
+        return ((playerScore > 21 && bankSCore > 21) || (playerScore <= 21 && playerScore >= bankSCore) || bankSCore > 21) ? "Player wins!" : "Bank wins!";
     }
 }
